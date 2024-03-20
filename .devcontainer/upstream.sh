a5aff84ee52ee638d1a1bb05515165ec550e1668
James Wilson <james@snapxr.io>
2024-03-19 22:18:46 -0400

    Renamed libs folders to upstream for consistency

--- .devcontainer/upstream.sh
+++ .devcontainer/upstream.sh
@@ -2,12 +2,12 @@
 # dos2unix flecs.sh to clear newline characters from the script file #windows-can-make-me-sad
 
 # Copy flecs src to library folder
-cp /opt/flecs-3.2.7/flecs.h /workspaces/zig-graphics-experiments/libs/flecs/libs/flecs
-cp /opt/flecs-3.2.7/flecs.c /workspaces/zig-graphics-experiments/libs/flecs/libs/flecs
+cp /opt/flecs-3.2.7/flecs.h /workspaces/zig-graphics-experiments/libs/flecs/upstream/flecs
+cp /opt/flecs-3.2.7/flecs.c /workspaces/zig-graphics-experiments/libs/flecs/upstream/flecs
+cp /opt/flecs-3.2.7/LICENSE /workspaces/zig-graphics-experiments/libs/flecs/upstream/flecs
 
 IMGUI_VERSION="1.90.4"
 cp -r /opt/imgui-${IMGUI_VERSION}/*.h /opt/imgui-${IMGUI_VERSION}/*.cpp /workspaces/zig-graphics-experiments/libs/imgui/upstream
-cp -r /opt/imgui-${IMGUI_VERSION}/backends /workspaces/zig-graphics-experiments/libs/imgui/upstream/backends
 cp -r /opt/imgui-${IMGUI_VERSION}/LICENSE.txt /workspaces/zig-graphics-experiments/libs/imgui/upstream/
 
 CIMGUI_VERSION="1.53.1"
